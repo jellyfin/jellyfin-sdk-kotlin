9c9fe1f87eaf8ef48d635811b32000e4f7739be2
Luke Pulverenti <luke.pulverenti@gmail.com>
2015-01-27 15:32:54 -0500

    remove support for connecting without /mediabrowser for now

--- src/mediabrowser/apiinteraction/BaseApiClient.java
+++ src/mediabrowser/apiinteraction/BaseApiClient.java
@@ -220,7 +220,7 @@ public abstract class BaseApiClient implements IDisposable
 	*/
 	public final String getApiUrl()
 	{
-		return getServerAddress();
+		return getServerAddress() + "/mediabrowser";
 	}
 
 	/** 
