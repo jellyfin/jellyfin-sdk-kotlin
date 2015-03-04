fdaaa31e236fc2c26ce698beda794c9a8571f4e5
Luke Pulverenti <luke.pulverenti@gmail.com>
2015-03-03 21:59:50 -0500

    start on playback manager methods

--- src/mediabrowser/apiinteraction/playback/PlaybackManager.java
+++ src/mediabrowser/apiinteraction/playback/PlaybackManager.java
@@ -1,7 +1,32 @@
 package mediabrowser.apiinteraction.playback;
 
-/**
- * Created by Luke on 3/3/2015.
- */
+import mediabrowser.apiinteraction.ApiClient;
+import mediabrowser.apiinteraction.EmptyResponse;
+import mediabrowser.apiinteraction.device.IDevice;
+import mediabrowser.apiinteraction.sync.data.ILocalAssetManager;
+import mediabrowser.model.logging.ILogger;
+import mediabrowser.model.session.PlaybackProgressInfo;
+import mediabrowser.model.session.PlaybackStartInfo;
+
 public class PlaybackManager {
+
+    private ILocalAssetManager localAssetManager;
+    private ILogger logger;
+    private IDevice device;
+
+    public void reportPlaybackStart(PlaybackStartInfo info, boolean isOffline, ApiClient apiClient, EmptyResponse response)
+    {
+        if (!isOffline)
+        {
+            apiClient.ReportPlaybackStartAsync(info, response);
+        }
+    }
+
+    public void reportPlaybackProgress(PlaybackProgressInfo info, boolean isOffline, ApiClient apiClient, EmptyResponse response)
+    {
+        if (!isOffline)
+        {
+            apiClient.ReportPlaybackProgressAsync(info, response);
+        }
+    }
 }
