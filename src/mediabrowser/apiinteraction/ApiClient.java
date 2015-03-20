28d252cc8141b3315851c9e9bb66a3572963bf6a
Luke Pulverenti <luke.pulverenti@gmail.com>
2015-03-20 16:09:42 -0400

    reactivate socket reporting

--- src/mediabrowser/apiinteraction/ApiClient.java
+++ src/mediabrowser/apiinteraction/ApiClient.java
@@ -1138,8 +1138,8 @@ public class ApiClient extends BaseApiClient {
         }
 
         if (apiWebSocket != null && apiWebSocket.IsWebSocketOpen()){
-            //apiWebSocket.SendWebSocketMessage("ReportPlaybackProgress", info, response);
-            //return;
+            apiWebSocket.SendWebSocketMessage("ReportPlaybackProgress", info, response);
+            return;
         }
 
         String url = GetApiUrl("Sessions/Playing/Progress");
