06415cb6a46c2d3dd60312f0fea5003a5347a0d0
dkanada <dkanada@users.noreply.github.com>
2019-08-14 15:59:34 -0700

    fix build issue and remove method

--- android/src/main/java/org/jellyfin/apiclient/interaction/AndroidCredentialProvider.java
+++ android/src/main/java/org/jellyfin/apiclient/interaction/AndroidCredentialProvider.java
@@ -3,7 +3,6 @@ package org.jellyfin.apiclient.interaction;
 import org.jellyfin.apiclient.model.apiclient.ServerCredentials;
 import org.jellyfin.apiclient.model.logging.ILogger;
 import org.jellyfin.apiclient.model.serialization.IJsonSerializer;
-import org.jellyfin.apiclient.interaction.ICredentialProvider;
 import android.content.Context;
 import android.content.SharedPreferences;
 
@@ -22,46 +21,24 @@ public class AndroidCredentialProvider implements ICredentialProvider {
     }
 
     private static SharedPreferences getSharedPreferences(Context context) {
-
         return context.getSharedPreferences("AndroidCredentialProvider", Context.MODE_PRIVATE);
     }
 
     @Override
     public ServerCredentials GetCredentials() {
-
         String json = getSharedPreferences(context).getString(settingsKey, "{}");
-
         return jsonSerializer.DeserializeFromString(json, ServerCredentials.class);
     }
 
     @Override
     public void SaveCredentials(ServerCredentials credentials) {
-
-        // We need an Editor object to make preference changes.
-        // All objects are from android.context.Context
         SharedPreferences settings = getSharedPreferences(context);
         SharedPreferences.Editor editor = settings.edit();
-
         editor.putString(settingsKey, jsonSerializer.SerializeToString(credentials));
 
-        // Commit the edits!
         boolean saved = editor.commit();
-
         if (!saved) {
             logger.Error("SharedPreferences.Editor failed to save credentials!");
         }
-    }
-
-    public static void Save(String credentialsJson, Context context) {
-
-        // We need an Editor object to make preference changes.
-        // All objects are from android.context.Context
-        SharedPreferences settings = getSharedPreferences(context);
-        SharedPreferences.Editor editor = settings.edit();
-
-        editor.putString(settingsKey, credentialsJson);
-
-        // Commit the edits!
-        boolean saved = editor.commit();
     }
 }
