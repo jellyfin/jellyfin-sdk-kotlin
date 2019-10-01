3d049c4785764630b0e7c80bfc5cefeb21d9cd94
Bill Thornton <billt2006@gmail.com>
2019-09-30 23:29:00 -0400

    Add version to ServerInfo

--- library/src/main/java/org/jellyfin/apiclient/model/apiclient/ServerInfo.java
+++ library/src/main/java/org/jellyfin/apiclient/model/apiclient/ServerInfo.java
@@ -1,70 +1,80 @@
 package org.jellyfin.apiclient.model.apiclient;
 
-import org.jellyfin.apiclient.model.system.*;
+import org.jellyfin.apiclient.model.system.PublicSystemInfo;
 
-public class ServerInfo
-{
-	private String Name;
-	public final String getName()
-	{
-		return Name;
-	}
-	public final void setName(String value)
-	{
-		Name = value;
-	}
-	private String Id;
-	public final String getId()
-	{
-		return Id;
-	}
-	public final void setId(String value)
-	{
-		Id = value;
-	}
-	private String Address;
-	public final String getAddress()
-	{
-		return Address;
-	}
-	public final void setAddress(String value)
-	{
-		Address = value;
-	}
-	private String UserId;
-	public final String getUserId()
-	{
-		return UserId;
-	}
-	public final void setUserId(String value)
-	{
-		UserId = value;
-	}
-	private String AccessToken;
-	public final String getAccessToken()
-	{
-		return AccessToken;
-	}
-	public final void setAccessToken(String value)
-	{
-		AccessToken = value;
-	}
-	private java.util.Date DateLastAccessed = new java.util.Date(0);
-	public final java.util.Date getDateLastAccessed()
-	{
-		return DateLastAccessed;
-	}
-	public final void setDateLastAccessed(java.util.Date value)
-	{
-		DateLastAccessed = value;
-	}
+import java.util.Date;
 
-	public final void ImportInfo(PublicSystemInfo systemInfo)
-	{
-		if (systemInfo == null) {
-			throw new IllegalArgumentException();
-		}
-		setName(systemInfo.getServerName());
-		setId(systemInfo.getId());
-	}
+public class ServerInfo {
+    private String name;
+    private String id;
+    private String address;
+    private String version;
+    private String userId;
+    private String accessToken;
+    private Date dateLastAccessed = new Date(0);
+
+    public final String getName() {
+        return name;
+    }
+
+    public final void setName(String value) {
+        name = value;
+    }
+
+    public final String getId() {
+        return id;
+    }
+
+    public final void setId(String value) {
+        id = value;
+    }
+
+    public final String getAddress() {
+        return address;
+    }
+
+    public final void setAddress(String value) {
+        address = value;
+    }
+
+    public final String getVersion() {
+        return version;
+    }
+
+    public final void setVersion(String version) {
+        this.version = version;
+    }
+
+    public final String getUserId() {
+        return userId;
+    }
+
+    public final void setUserId(String value) {
+        userId = value;
+    }
+
+    public final String getAccessToken() {
+        return accessToken;
+    }
+
+    public final void setAccessToken(String value) {
+        accessToken = value;
+    }
+
+    public final Date getDateLastAccessed() {
+        return dateLastAccessed;
+    }
+
+    public final void setDateLastAccessed(Date value) {
+        dateLastAccessed = value;
+    }
+
+    public final void ImportInfo(PublicSystemInfo systemInfo) {
+        if (systemInfo == null) {
+            throw new IllegalArgumentException();
+        }
+        setName(systemInfo.getServerName());
+        setId(systemInfo.getId());
+        setVersion(systemInfo.getVersion());
+    }
 }
