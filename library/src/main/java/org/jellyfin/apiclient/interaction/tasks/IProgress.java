4c3bb7142788473f0a02b0201e6dfb8a979b344e
Niels van Velzen <git@ndat.nl>
2020-07-31 20:55:13 +0200

    Remove UploadFile function from ApiClient

--- library/src/main/java/org/jellyfin/apiclient/interaction/tasks/IProgress.java
+++
@@ -1,9 +0,0 @@
-package org.jellyfin.apiclient.interaction.tasks;
-
-public interface IProgress<T> {
-
-    void report(T progress);
-    void reportComplete();
-    void reportCancelled();
-    void reportError(Exception exception);
-}
