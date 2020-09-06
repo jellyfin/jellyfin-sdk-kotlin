fac5e1b818800e66d6aa1994dad4a2b5dd009443
Niels van Velzen <git@ndat.nl>
2020-09-06 14:32:37 +0200

    Add UserApi.authenticateUserByName extension

---
+++ api/src/main/kotlin/org/jellyfin/apiclient/api/client/extensions/UserApiExtensions.kt
@@ -0,0 +1,14 @@
+package org.jellyfin.apiclient.api.client.extensions
+
+import org.jellyfin.apiclient.api.operations.UserApi
+import org.jellyfin.apiclient.model.AuthenticateUserByName
+
+/**
+ * Extension function for the authenticateUserByName operation that accepts the username and password directly
+ */
+suspend inline fun UserApi.authenticateUserByName(username: String, password: String) = authenticateUserByName(
+	data = AuthenticateUserByName(
+		username = username,
+		password = password
+	)
+)
