471a719e9cea617e70a5a7cd44e37dd1f1188a45
Niels van Velzen <git@ndat.nl>
2020-10-04 17:22:31 +0200

    Remove old logger (replaced with slf4j)

--- library/src/main/java/org/jellyfin/apiclient/JellyfinOptions.kt
+++ library/src/main/java/org/jellyfin/apiclient/JellyfinOptions.kt
@@ -2,25 +2,20 @@ package org.jellyfin.apiclient
 
 import org.jellyfin.apiclient.discovery.DiscoveryBroadcastAddressesProvider
 import org.jellyfin.apiclient.discovery.JavaNetBroadcastAddressesProvider
-import org.jellyfin.apiclient.logging.ILogger
-import org.jellyfin.apiclient.logging.NullLogger
 import org.jellyfin.apiclient.model.ClientInfo
 import org.jellyfin.apiclient.model.DeviceInfo
 
 data class JellyfinOptions(
-	val logger: ILogger,
 	val discoverBroadcastAddressesProvider: DiscoveryBroadcastAddressesProvider,
 	val clientInfo: ClientInfo?,
 	val deviceInfo: DeviceInfo?
 ) {
 	class Builder {
-		var logger: ILogger = NullLogger()
 		var discoveryBroadcastAddressesProvider: DiscoveryBroadcastAddressesProvider = JavaNetBroadcastAddressesProvider()
 		var clientInfo: ClientInfo? = null
 		var deviceInfo: DeviceInfo? = null
 
 		fun build() = JellyfinOptions(
-			logger,
 			discoveryBroadcastAddressesProvider,
 			clientInfo,
 			deviceInfo
--- library/src/main/java/org/jellyfin/apiclient/logging/ILogger.kt
+++
@@ -1,8 +0,0 @@
-package org.jellyfin.apiclient.logging
-
-interface ILogger {
-	fun debug(str: String, vararg args: Any)
-	fun info(str: String, vararg args: Any)
-	fun error(str: String, vararg args: Any)
-	fun error(str: String, exception: Exception, vararg args: Any)
-}
