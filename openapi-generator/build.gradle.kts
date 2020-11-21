ffa76ba24f5f5f545cbf36f83601acf4db23af00
Niels van Velzen <git@ndat.nl>
2020-11-21 16:16:04 +0100

    Add download and update tasks for OpenAPI spec
    Uses repo.jellyfin.org to retrieve stable/unstable specs

--- openapi-generator/build.gradle.kts
+++ openapi-generator/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
 	kotlin("jvm")
 	id("application")
+
+	id("de.undercouch.download")
 }
 
 application {
@@ -30,13 +32,25 @@ dependencies {
 	testImplementation(Dependencies.Kotlin.Test.junit)
 }
 
-task("generateSources", JavaExec::class) {
+val openApiFile = file("../openapi.json")
+tasks.register("generateSources", JavaExec::class) {
 	main = application.mainClassName
 	classpath = sourceSets.main.get().runtimeClasspath
 
 	args = mapOf(
-		"openApiFile" to file("../openapi.json"),
+		"openApiFile" to openApiFile,
 		"apiOutputDir" to file("../jellyfin-api/src/main/kotlin-generated"),
 		"modelsOutputDir" to file("../jellyfin-model/src/main/kotlin-generated")
 	).map { listOf("--${it.key}", it.value.toString()) }.flatten()
+}
+
+arrayOf("stable", "unstable").forEach { flavor ->
+	tasks.register("downloadApiSpec${flavor.capitalize()}", de.undercouch.gradle.tasks.download.Download::class) {
+		src("https://repo.jellyfin.org/releases/openapi/jellyfin-openapi-${flavor}.json")
+		dest(openApiFile)
+	}
+
+	tasks.register("updateApiSpec${flavor.capitalize()}") {
+		dependsOn("downloadApiSpec${flavor.capitalize()}", "generateSources")
+	}
 }
