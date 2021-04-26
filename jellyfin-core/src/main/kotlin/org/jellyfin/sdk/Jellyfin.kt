249d313a636d190f94b94daaf37a6de9f150a0c0
Niels van Velzen <git@ndat.nl>
2021-04-26 17:19:26 +0200

    Add deviceInfo and clientInfo getters in Jellyfin class

--- jellyfin-core/src/main/kotlin/org/jellyfin/sdk/Jellyfin.kt
+++ jellyfin-core/src/main/kotlin/org/jellyfin/sdk/Jellyfin.kt
@@ -8,10 +8,23 @@ import org.jellyfin.sdk.model.DeviceInfo
 import org.jellyfin.sdk.model.ServerVersion
 
 public class Jellyfin(
-	private val options: JellyfinOptions
+	private val options: JellyfinOptions,
 ) {
 	public constructor(initOptions: JellyfinOptions.Builder.() -> Unit) : this(JellyfinOptions.build(initOptions))
 
+	/**
+	 * Get the device information set when creating the Jellyfin instance or null if not set.
+	 */
+	public val deviceInfo: DeviceInfo? get() = options.deviceInfo
+
+	/**
+	 * Get the client information set when creating the Jellyfin instance or null if not set.
+	 */
+	public val clientInfo: ClientInfo? get() = options.clientInfo
+
+	/**
+	 * Get the discovery service to help with normalizing server addresses and find servers in the local network.
+	 */
 	public val discovery: DiscoveryService by lazy {
 		DiscoveryService(this, options.discoverBroadcastAddressesProvider)
 	}
@@ -20,12 +33,14 @@ public class Jellyfin(
 	 * Create a new API instance to use in API services.
 	 * The [clientInfo] and [deviceInfo] parameters are required when not passed as option in [JellyfinOptions].
 	 * The [baseUrl] is only required when HTTP calls are made.
+	 *
+	 * Throws an [IllegalStateException] when the client or device information is missing.
 	 */
 	public fun createApi(
 		baseUrl: String? = null,
 		accessToken: String? = null,
 		clientInfo: ClientInfo? = options.clientInfo,
-		deviceInfo: DeviceInfo? = options.deviceInfo
+		deviceInfo: DeviceInfo? = options.deviceInfo,
 	): KtorClient {
 		checkNotNull(clientInfo) {
 			"ClientInfo needs to be set when calling createApi() or by providing it when constructing the Jellyfin instance"
