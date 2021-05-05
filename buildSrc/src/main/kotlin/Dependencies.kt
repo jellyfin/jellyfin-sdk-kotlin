f4f40e6dd43fa20a43afec5e0ee30105bbe009a3
Niels van Velzen <nielsvanvelzen@users.noreply.github.com>
2021-05-05 13:12:04 +0200

    Update Dokka to version 1.4.32 (#241)

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -11,7 +11,7 @@ object Plugins {
 		const val binaryCompatibilityValidatorVersion = "0.5.0"
 		const val detekt = "1.17.0-RC2"
 		const val nexusPublish = "1.0.0"
-		const val dokka = "1.4.30"
+		const val dokka = "1.4.32"
 		const val androidBuildTools = "4.2.0"
 	}
 
