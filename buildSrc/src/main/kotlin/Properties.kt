05b48d202f7b6a23498f8adee176728eb5081a0f
Niels van Velzen <git@ndat.nl>
2021-05-28 22:15:58 +0200

    Add `jellyfin.branch` property to create versions from a branch name

--- buildSrc/src/main/kotlin/Properties.kt
+++ buildSrc/src/main/kotlin/Properties.kt
@@ -7,6 +7,17 @@ fun Project.getProperty(name: String): String? {
 	// sample.var --> SAMPLE_VAR
 	val environmentName = name.toUpperCase().replace(".", "_")
 	val value = findProperty(name)?.toString() ?: System.getenv(environmentName) ?: null
-	logger.info("getProperty($name): $environmentName - found=${!value.isNullOrBlank()}")
+	logger.debug("getProperty($name): $environmentName - found=${!value.isNullOrBlank()}")
 	return value
+}
+
+/**
+ * Helper function to create the SDK version using the `jellyfin.version` and `jellyfin.branch` properties.
+ * Uses [getProperty] to retrieve the properties. Defaults to `latest-SNAPSHOT` if both properties are not set.
+ */
+fun Project.createVersion(): String {
+	val jellyfinVersion = getProperty("jellyfin.version")?.removePrefix("v")
+	val jellyfinBranch = getProperty("jellyfin.branch")?.let { "$it-SNAPSHOT" }
+
+	return jellyfinVersion ?: jellyfinBranch ?: "latest-SNAPSHOT"
 }
