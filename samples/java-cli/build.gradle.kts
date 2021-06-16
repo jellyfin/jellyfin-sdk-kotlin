b0927b962ccec263ff5dcedaee41d067a8aacc9f
Niels van Velzen <git@ndat.nl>
2021-06-16 10:59:34 +0200

    Downgrade Java sample to use Java 8 features

--- samples/java-cli/build.gradle.kts
+++ samples/java-cli/build.gradle.kts
@@ -6,6 +6,11 @@ application {
 	mainClass.set("org.jellyfin.sample.cli.Main")
 }
 
+java {
+	sourceCompatibility = JavaVersion.VERSION_1_8
+	targetCompatibility = JavaVersion.VERSION_1_8
+}
+
 dependencies {
 	// Depend on the library project
 	implementation(projects.jellyfinCore)
--- samples/java-cli/src/main/java/org/jellyfin/sample/cli/Main.java
+++ samples/java-cli/src/main/java/org/jellyfin/sample/cli/Main.java
@@ -2,6 +2,7 @@ package org.jellyfin.sample.cli;
 
 import org.jellyfin.sdk.Jellyfin;
 import org.jellyfin.sdk.JellyfinOptions;
+import org.jellyfin.sdk.api.client.KtorClient;
 import org.jellyfin.sdk.api.client.compatibility.JavaDataCallback;
 import org.jellyfin.sdk.api.operations.UserApi;
 import org.jellyfin.sdk.model.ClientInfo;
@@ -9,24 +10,25 @@ import org.jellyfin.sdk.model.DeviceInfo;
 import org.jellyfin.sdk.model.api.AuthenticateUserByName;
 import org.jellyfin.sdk.model.api.AuthenticationResult;
 import org.jetbrains.annotations.Nullable;
+import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.concurrent.CountDownLatch;
 
 public class Main {
     public static void main(String[] args) {
-        var options = new JellyfinOptions.Builder();
+        JellyfinOptions.Builder options = new JellyfinOptions.Builder();
         options.setClientInfo(new ClientInfo("Jellyfin Sample: Kotlin CLI", "DEV"));
         options.setDeviceInfo(new DeviceInfo("cli", "cli"));
-        var jellyfin = new Jellyfin(options);
+        Jellyfin jellyfin = new Jellyfin(options);
 
         // TODO Replace below with a proper CLI that compares to the kotlin-cli sample
-        var logger = LoggerFactory.getLogger(Main.class);
-        var client = jellyfin.createApi("https://demo.jellyfin.org/stable/");
+        Logger logger = LoggerFactory.getLogger(Main.class);
+        KtorClient client = jellyfin.createApi("https://demo.jellyfin.org/stable/");
 
-        var latch = new CountDownLatch(1);
-        var userApi = new UserApi(client);
-        userApi.authenticateUserByName(new AuthenticateUserByName("demo", null, ""), new JavaDataCallback<>() {
+        CountDownLatch latch = new CountDownLatch(1);
+        UserApi userApi = new UserApi(client);
+        userApi.authenticateUserByName(new AuthenticateUserByName("demo", null, ""), new JavaDataCallback<AuthenticationResult>() {
             @Override
             public void onData(@Nullable AuthenticationResult data) {
                 assert data != null;
