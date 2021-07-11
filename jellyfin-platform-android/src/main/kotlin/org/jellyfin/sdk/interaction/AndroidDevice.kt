fbc3ccbca7c2a1f18b0b4a8776e243a2374257c3
Nazar78 <nazar@teanazar.com>
2021-07-12 03:18:09 +0800

    Ensure device name has no special characters or fallback to manufacturer and model else the connection will fail

--- jellyfin-platform-android/src/main/kotlin/org/jellyfin/sdk/interaction/AndroidDevice.kt
+++ jellyfin-platform-android/src/main/kotlin/org/jellyfin/sdk/interaction/AndroidDevice.kt
@@ -10,9 +10,21 @@ import org.jellyfin.sdk.model.DeviceInfo
 public fun androidDevice(context: Context): DeviceInfo {
 	val id = Settings.Secure.getString(context.contentResolver, Settings.Secure.ANDROID_ID)
 
+	// Ensure device name has no special characters or fallback to manufacturer and model else the connection will fail
+	val testDeviceName = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N_MR1) {
+		val deviceName = Settings.Global.getString(context.contentResolver, Settings.Global.DEVICE_NAME)
+		if (deviceName.matches("([^\\x20-\\x7E])".toRegex())) {
+			deviceName
+		} else {
+			null
+		}
+	} else {
+		null
+	}
+
 	// Use name from device settings
-	val name = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N_MR1) {
-		Settings.Global.getString(context.contentResolver, Settings.Global.DEVICE_NAME)
+	val name = if (testDeviceName != null) {
+		testDeviceName
 	} else {
 		// Concatenate the name based on manufacturer and model
 		val manufacturer = Build.MANUFACTURER
