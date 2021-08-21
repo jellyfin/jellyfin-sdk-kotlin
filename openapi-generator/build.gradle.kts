e846b6f437a38e09aebb98cf33ea8109ea177864
Niels van Velzen <git@ndat.nl>
2021-08-21 15:26:52 +0200

    Add verify function to openapi-generator

--- openapi-generator/build.gradle.kts
+++ openapi-generator/build.gradle.kts
@@ -32,22 +32,35 @@ dependencies {
 	testImplementation(libs.kotlin.test.junit)
 }
 
-val openApiFile = file("../openapi.json")
+val defaultConfig = mapOf(
+	"openApiFile" to file("../openapi.json"),
+	"apiOutputDir" to file("../jellyfin-api/src/main/kotlin-generated"),
+	"modelsOutputDir" to file("../jellyfin-model/src/main/kotlin-generated")
+)
+
 tasks.register("generateSources", JavaExec::class) {
 	mainClass.set(application.mainClass)
 	classpath = sourceSets.main.get().runtimeClasspath
 
-	args = mapOf(
-		"openApiFile" to openApiFile,
-		"apiOutputDir" to file("../jellyfin-api/src/main/kotlin-generated"),
-		"modelsOutputDir" to file("../jellyfin-model/src/main/kotlin-generated")
-	).map { listOf("--${it.key}", it.value.toString()) }.flatten()
+	args = defaultConfig
+		.map { listOf("--${it.key}", it.value.toString()) }
+		.flatten()
+}
+
+tasks.register("verifySources", JavaExec::class) {
+	mainClass.set(application.mainClass)
+	classpath = sourceSets.main.get().runtimeClasspath
+
+	args = defaultConfig
+		.plus("verify" to "true")
+		.map { listOf("--${it.key}", it.value.toString()) }
+		.flatten()
 }
 
 arrayOf("stable", "unstable").forEach { flavor ->
 	tasks.register("downloadApiSpec${flavor.capitalize()}", Download::class) {
 		src("https://repo.jellyfin.org/releases/openapi/jellyfin-openapi-${flavor}.json")
-		dest(openApiFile)
+		dest(defaultConfig["openApiFile"])
 	}
 
 	tasks.register("updateApiSpec${flavor.capitalize()}") {
--- openapi-generator/src/main/kotlin/org/jellyfin/openapi/Main.kt
+++ openapi-generator/src/main/kotlin/org/jellyfin/openapi/Main.kt
@@ -3,6 +3,7 @@ package org.jellyfin.openapi
 import org.jellyfin.openapi.hooks.hooksModule
 import org.koin.dsl.koinApplication
 import java.io.File
+import kotlin.system.exitProcess
 
 fun main(vararg args: String) {
 	// Read arguments
@@ -10,17 +11,30 @@ fun main(vararg args: String) {
 	val openApiFile by arguments
 	val apiOutputDir by arguments
 	val modelsOutputDir by arguments
+	val verify = arguments.containsKey("verify")
 
 	// Read OpenAPI json
 	val openApiJson = File(openApiFile).readText()
 
 	// Start Koin
 	val koin = koinApplication { modules(mainModule, hooksModule) }.koin
+	val generator = koin.get<Generator>()
 
-	// Generate OpenAPI output
-	koin.get<Generator>().generate(
-		openApiJson = openApiJson,
-		apiOutputDir = File(apiOutputDir),
-		modelsOutputDir = File(modelsOutputDir)
-	)
+	if (verify) {
+		// Verify OpenAPI output
+		val valid = generator.verify(
+			openApiJson = openApiJson,
+			apiOutputDir = File(apiOutputDir),
+			modelsOutputDir = File(modelsOutputDir)
+		)
+
+		if (!valid) exitProcess(1)
+	} else {
+		// Generate OpenAPI output
+		generator.generate(
+			openApiJson = openApiJson,
+			apiOutputDir = File(apiOutputDir),
+			modelsOutputDir = File(modelsOutputDir)
+		)
+	}
 }
