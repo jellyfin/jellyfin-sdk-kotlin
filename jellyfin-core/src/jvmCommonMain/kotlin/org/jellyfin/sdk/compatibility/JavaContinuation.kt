60077fb2d66e1fe4c27b737ad7c9c45bf99a48e0
Miguel Álvarez Díez <miguelwork92@gmail.com>
2021-12-24 15:11:20 +0100

    fix implementation
    Signed-off-by: Miguel Álvarez Díez <miguelwork92@gmail.com>

--- jellyfin-core/src/jvmCommonMain/kotlin/org/jellyfin/sdk/compatibility/JavaContinuation.kt
+++ jellyfin-core/src/jvmCommonMain/kotlin/org/jellyfin/sdk/compatibility/JavaContinuation.kt
@@ -1,9 +1,7 @@
 package org.jellyfin.sdk.compatibility
 
-import kotlinx.coroutines.CoroutineDispatcher
-import kotlinx.coroutines.Dispatchers
 import kotlin.coroutines.Continuation
-import kotlin.coroutines.CoroutineContext
+import kotlin.coroutines.EmptyCoroutineContext
 
 /**
  * Compatibility class to use Kotlin suspend functions in Java files.
@@ -14,17 +12,13 @@ public object JavaContinuation {
 	}
 	/**
 	 * .
-	 * @param resultCallback Called when the function ends with error or data.
+	 * @param returnCallback Called when the function ends with error or data.
 	 */
-	@JvmOverloads
 	@JvmStatic
-	public fun <R> get(returnCallback: ReturnCallback<R>, dispatcher: CoroutineDispatcher = Dispatchers.Default): Continuation<R> {
+	public fun <R> get(returnCallback: ReturnCallback<R>): Continuation<R> {
 		return object : Continuation<R> {
-			override val context: CoroutineContext
-				get() = dispatcher
-			override fun resumeWith(result: Result<R>) {
-				returnCallback.onReturn(result.exceptionOrNull(), result.getOrNull())
-			}
+			override val context = EmptyCoroutineContext
+			override fun resumeWith(result: Result<R>) = returnCallback.onReturn(result.exceptionOrNull(), result.getOrNull())
 		}
 	}
 }
