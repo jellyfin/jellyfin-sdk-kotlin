c2680983e86328e7e442a1387bc215845d3a6096
Niels van Velzen <git@ndat.nl>
2021-12-27 16:06:32 +0100

    Add DisplayPreferencesDtoNullabilityFixHook
    A hook that modifies the type of the "customPrefs" property in "DisplayPreferencesDto" to fix a nullability issue. In the 10.7 API specification the value of the map is incorrectly labelled as not-null. This hook changes the type to the exact type emitted from the 10.8 alpha API specification.

---
+++ openapi-generator/src/main/kotlin/org/jellyfin/openapi/hooks/model/DisplayPreferencesDtoNullabilityFixHook.kt
@@ -0,0 +1,21 @@
+package org.jellyfin.openapi.hooks.model
+
+import com.squareup.kotlinpoet.typeNameOf
+import io.swagger.v3.oas.models.media.Schema
+import org.jellyfin.openapi.builder.openapi.OpenApiTypeBuilder
+import org.jellyfin.openapi.hooks.ModelTypePath
+import org.jellyfin.openapi.hooks.TypeBuilderHook
+import org.jellyfin.openapi.hooks.TypePath
+
+/**
+ * A hook that modifies the type of the "customPrefs" property in "DisplayPreferencesDto" to fix a nullability issue.
+ * In the 10.7 API specification the value of the map is incorrectly labelled as not-null.
+ * This hook changes the type to the exact type emitted from the 10.8 alpha API specification.
+ */
+@OptIn(ExperimentalStdlibApi::class)
+class DisplayPreferencesDtoNullabilityFixHook : TypeBuilderHook {
+	override fun onBuildType(path: TypePath, schema: Schema<*>, typeBuilder: OpenApiTypeBuilder) = when (path) {
+		ModelTypePath("DisplayPreferencesDto", "customPrefs") -> typeNameOf<Map<String, String?>>()
+		else -> null
+	}
+}
