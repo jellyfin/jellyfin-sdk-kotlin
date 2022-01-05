c28f76ff69ed03e85bc0c903c144000ee62324f1
Niels van Velzen <nielsvanvelzen@users.noreply.github.com>
2022-01-05 18:56:38 +0100

    Support username and password arguments in kotlin-cli sample (#350)

--- samples/kotlin-cli/src/main/kotlin/org/jellyfin/sample/cli/Options.kt
+++ samples/kotlin-cli/src/main/kotlin/org/jellyfin/sample/cli/Options.kt
@@ -3,6 +3,11 @@ package org.jellyfin.sample.cli
 import com.github.ajalt.clikt.core.ParameterHolder
 import com.github.ajalt.clikt.parameters.options.option
 import com.github.ajalt.clikt.parameters.options.required
+import kotlinx.coroutines.runBlocking
+import org.jellyfin.sdk.Jellyfin
+import org.jellyfin.sdk.api.client.ApiClient
+import org.jellyfin.sdk.api.client.extensions.authenticateUserByName
+import org.jellyfin.sdk.api.client.extensions.userApi
 
 fun ParameterHolder.serverOption() = option(
 	"-s", "--server",
@@ -10,8 +15,38 @@ fun ParameterHolder.serverOption() = option(
 	envvar = "JELLYFIN_SERVER"
 ).required()
 
-fun ParameterHolder.tokenOption() = option(
-	"-t", "--token",
-	help = "Access token",
-	envvar = "JELLYFIN_TOKEN"
-).required()
+fun ParameterHolder.apiInstanceHolder(jellyfin: Jellyfin): Lazy<ApiClient> {
+	val server = serverOption().also(::registerOption)
+
+	val token = option(
+		"-t", "--token",
+		help = "Access token",
+		envvar = "JELLYFIN_TOKEN"
+	).also(::registerOption)
+
+	val username = option(
+		"-u", "--username",
+		help = "Username"
+	).also(::registerOption)
+
+	val password = option(
+		"-p", "--password",
+		help = "Password"
+	).also(::registerOption)
+
+	return lazy {
+		runBlocking {
+			// Create instance
+			val api = jellyfin.createApi(baseUrl = server.value, accessToken = token.value)
+
+			// Authenticate manually if access token is not provided
+			if (api.accessToken == null && username.value != null) {
+				val response by api.userApi.authenticateUserByName(username.value.orEmpty(), password.value.orEmpty())
+				api.accessToken = response.accessToken
+			}
+
+			// Return
+			api
+		}
+	}
+}
--- samples/kotlin-cli/src/main/kotlin/org/jellyfin/sample/cli/command/Observe.kt
+++ samples/kotlin-cli/src/main/kotlin/org/jellyfin/sample/cli/command/Observe.kt
@@ -2,8 +2,7 @@ package org.jellyfin.sample.cli.command
 
 import com.github.ajalt.clikt.core.CliktCommand
 import kotlinx.coroutines.runBlocking
-import org.jellyfin.sample.cli.serverOption
-import org.jellyfin.sample.cli.tokenOption
+import org.jellyfin.sample.cli.apiInstanceHolder
 import org.jellyfin.sdk.Jellyfin
 import org.jellyfin.sdk.api.client.extensions.webSocket
 import org.jellyfin.sdk.model.socket.ActivityLogEntryStartMessage
@@ -11,13 +10,11 @@ import org.jellyfin.sdk.model.socket.ScheduledTasksInfoStartMessage
 import org.jellyfin.sdk.model.socket.SessionsStartMessage
 
 class Observe(
-	private val jellyfin: Jellyfin
+	jellyfin: Jellyfin
 ) : CliktCommand("Create a WebSocket connection and listen to all events") {
-	private val server by serverOption()
-	private val token by tokenOption()
+	private val api by apiInstanceHolder(jellyfin)
 
 	override fun run() = runBlocking {
-		val api = jellyfin.createApi(baseUrl = server, accessToken = token)
 		val webSocketApi = api.webSocket
 
 		println("Starting subscription")
