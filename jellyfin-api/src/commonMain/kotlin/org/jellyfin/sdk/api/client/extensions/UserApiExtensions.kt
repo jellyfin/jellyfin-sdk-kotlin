cbdc3f803d4d8f29866ec450601819e8e73614b3
Niels van Velzen <nielsvanvelzen@users.noreply.github.com>
2022-04-19 13:54:27 +0200

    Add extension function for authenticateWithQuickConnect that takes the secret directly (#382)

--- jellyfin-api/src/commonMain/kotlin/org/jellyfin/sdk/api/client/extensions/UserApiExtensions.kt
+++ jellyfin-api/src/commonMain/kotlin/org/jellyfin/sdk/api/client/extensions/UserApiExtensions.kt
@@ -4,16 +4,28 @@ import org.jellyfin.sdk.api.client.Response
 import org.jellyfin.sdk.api.operations.UserApi
 import org.jellyfin.sdk.model.api.AuthenticateUserByName
 import org.jellyfin.sdk.model.api.AuthenticationResult
+import org.jellyfin.sdk.model.api.QuickConnectDto
 
 /**
  * Extension function for the authenticateUserByName operation that accepts the username and password directly
  */
 public suspend inline fun UserApi.authenticateUserByName(
 	username: String,
-	password: String
+	password: String,
 ): Response<AuthenticationResult> = authenticateUserByName(
 	data = AuthenticateUserByName(
 		username = username,
 		pw = password
+	)
+)
+
+/**
+ * Extension function for the authenticateWithQuickConnect operation that accepts the secret directly
+ */
+public suspend inline fun UserApi.authenticateWithQuickConnect(
+	secret: String,
+): Response<AuthenticationResult> = authenticateWithQuickConnect(
+	data = QuickConnectDto(
+		secret = secret
 	)
 )
