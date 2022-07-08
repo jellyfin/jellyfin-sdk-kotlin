06b6f204ebdb49816cacaae482bb410e7ce5ab2f
Niels van Velzen <git@ndat.nl>
2022-07-08 11:06:53 +0200

    Allow client to change minimum server version
    This mostly affects the RecommendedServerDiscovery rejecting servers not meeting the hardcoded minimum server version, but the option value can be used by the client too.

--- jellyfin-core/src/androidMain/kotlin/org/jellyfin/sdk/JellyfinOptions.kt
+++ jellyfin-core/src/androidMain/kotlin/org/jellyfin/sdk/JellyfinOptions.kt
@@ -7,6 +7,7 @@ import org.jellyfin.sdk.api.sockets.OkHttpWebsocketSession
 import org.jellyfin.sdk.api.sockets.SocketConnectionFactory
 import org.jellyfin.sdk.model.ClientInfo
 import org.jellyfin.sdk.model.DeviceInfo
+import org.jellyfin.sdk.model.ServerVersion
 import org.jellyfin.sdk.util.ApiClientFactory
 
 public actual data class JellyfinOptions(
@@ -15,6 +16,7 @@ public actual data class JellyfinOptions(
 	public actual val deviceInfo: DeviceInfo?,
 	public actual val apiClientFactory: ApiClientFactory,
 	public actual val socketConnectionFactory: SocketConnectionFactory,
+	public actual val minimumServerVersion: ServerVersion,
 ) {
 	public actual class Builder {
 		public var context: Context? = null
@@ -22,6 +24,7 @@ public actual data class JellyfinOptions(
 		public var deviceInfo: DeviceInfo? = null
 		public var apiClientFactory: ApiClientFactory = ApiClientFactory(::KtorClient)
 		public var socketConnectionFactory: SocketConnectionFactory = SocketConnectionFactory(::OkHttpWebsocketSession)
+		public var minimumServerVersion: ServerVersion = Jellyfin.minimumVersion
 
 		public actual fun build(): JellyfinOptions = JellyfinOptions(
 			context = requireNotNull(context) {
@@ -31,6 +34,7 @@ public actual data class JellyfinOptions(
 			deviceInfo = deviceInfo ?: androidDevice(context!!),
 			apiClientFactory = apiClientFactory,
 			socketConnectionFactory = socketConnectionFactory,
+			minimumServerVersion = minimumServerVersion,
 		)
 	}
 
