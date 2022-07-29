3f839eb158db21c6622a6182e620f241bf7b24c3
Niels van Velzen <git@ndat.nl>
2022-07-29 21:20:00 +0200

    Remove catching Throwable exceptions
    This fixes the TooGenericExceptionCaught linter issue. We ignored it, but it actually ended up causing issues. When a coroutine is cancelled it often throws a CancellationException. This was caught and converted to an ApiClientException causing coroutine cancellation to stop working. This change removes the places that catch Throwable, and they may throw exceptions now. We'll need to watch this and possibly add new catch branches later.

--- jellyfin-api/src/commonMain/kotlin/org/jellyfin/sdk/api/client/RawResponse.kt
+++ jellyfin-api/src/commonMain/kotlin/org/jellyfin/sdk/api/client/RawResponse.kt
@@ -3,7 +3,6 @@ package org.jellyfin.sdk.api.client
 import io.ktor.utils.io.ByteReadChannel
 import kotlinx.serialization.SerializationException
 import mu.KotlinLogging
-import org.jellyfin.sdk.api.client.exception.ApiClientException
 import org.jellyfin.sdk.api.client.exception.InvalidContentException
 import org.jellyfin.sdk.api.client.util.ApiSerializer
 
@@ -15,15 +14,11 @@ public class RawResponse(
 	public suspend inline fun <reified T : Any> createContent(): T {
 		val logger = KotlinLogging.logger {}
 
-		@Suppress("TooGenericExceptionCaught")
 		return try {
 			ApiSerializer.decodeResponseBody(body)
 		} catch (err: SerializationException) {
 			logger.error(err) { "Deserialization failed" }
 			throw InvalidContentException("Deserialization failed", err)
-		} catch (err: Throwable) {
-			logger.error(err) { "Unknown error occurred!" }
-			throw ApiClientException("Unknown error occurred!", err)
 		}
 	}
 
