de183179c6febea41fe52e2958fc43352b2ca577
Niels van Velzen <git@ndat.nl>
2022-12-27 13:53:02 +0100

    Add extensions to Long, Int and Double to create Duration from ticks

---
+++ jellyfin-model/src/commonMain/kotlin/org/jellyfin/sdk/model/extensions/TicksExtensions.kt
@@ -0,0 +1,22 @@
+@file:Suppress("MagicNumber")
+
+package org.jellyfin.sdk.model.extensions
+
+import kotlin.time.Duration
+import kotlin.time.DurationUnit
+import kotlin.time.toDuration
+
+/**
+ * Returns a [Duration] equal to this [Long] number of ticks.
+ */
+public inline val Long.ticks: Duration get() = times(100L).toDuration(DurationUnit.NANOSECONDS)
+
+/**
+ * Returns a [Duration] equal to this [Int] number of ticks.
+ */
+public inline val Int.ticks: Duration get() = times(100).toDuration(DurationUnit.NANOSECONDS)
+
+/**
+ * Returns a [Duration] equal to this [Double] number of ticks.
+ */
+public inline val Double.ticks: Duration get() = times(100.0).toDuration(DurationUnit.NANOSECONDS)
