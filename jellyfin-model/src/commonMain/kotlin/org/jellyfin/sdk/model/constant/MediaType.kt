63b2a2bf192677cd25e1caadb3f300a6b3b45c15
Niels van Velzen <git@ndat.nl>
2024-03-26 15:41:14 +0100

    Remove manually model.constant package
    Replaced with auto-generated enums

--- jellyfin-model/src/commonMain/kotlin/org/jellyfin/sdk/model/constant/MediaType.kt
+++
@@ -1,9 +0,0 @@
-package org.jellyfin.sdk.model.constant
-
-// https://github.com/jellyfin/jellyfin/blob/v10.8.4/MediaBrowser.Model/Entities/MediaType.cs
-public object MediaType {
-	public const val Video: String = "Video"
-	public const val Audio: String = "Audio"
-	public const val Photo: String = "Photo"
-	public const val Book: String = "Book"
-}
