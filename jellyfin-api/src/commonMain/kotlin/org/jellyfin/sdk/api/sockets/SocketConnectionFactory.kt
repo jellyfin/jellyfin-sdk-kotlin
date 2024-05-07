965154091c233b08f8b8eadc5785515f7f6fbe58
Niels van Velzen <git@ndat.nl>
2024-05-07 13:29:46 +0200

    Address review feedback

--- jellyfin-api/src/commonMain/kotlin/org/jellyfin/sdk/api/sockets/SocketConnectionFactory.kt
+++ jellyfin-api/src/commonMain/kotlin/org/jellyfin/sdk/api/sockets/SocketConnectionFactory.kt
@@ -7,8 +7,5 @@ import org.jellyfin.sdk.api.client.HttpClientOptions
  * The factory to create a [SocketConnection].
  */
 public fun interface SocketConnectionFactory {
-	public fun create(
-		clientOptions: HttpClientOptions,
-		scope: CoroutineScope,
-	): SocketConnection
+	public fun create(clientOptions: HttpClientOptions, scope: CoroutineScope): SocketConnection
 }
--- jellyfin-api/src/commonMain/kotlin/org/jellyfin/sdk/api/sockets/SocketConnectionState.kt
+++ jellyfin-api/src/commonMain/kotlin/org/jellyfin/sdk/api/sockets/SocketConnectionState.kt
@@ -1,25 +1,25 @@
 package org.jellyfin.sdk.api.sockets
 
 /**
- * Possible states for a [SocketConnection]. This will start out as [DISCONNECTED] with no error set and switch to
- * [CONNECTING] when attempting to initialize a session. The [MESSAGE] state means that the socket is connected.
+ * Possible states for a [SocketConnection]. This will start out as [Disconnected] with no error set and switch to
+ * [Connecting] when attempting to initialize a session. The [Message] state means that the socket is connected.
  *
- * The server is always the first to send a message, so we don't have an intermediate state between [CONNECTING] and
- * [MESSAGE].
+ * The server is always the first to send a message, so we don't have an intermediate state between [Connecting] and
+ * [Message].
  */
 public sealed class SocketConnectionState {
 	/**
 	 * There is no current connection. This is the default state.
 	 */
-	public data class DISCONNECTED(val error: Throwable? = null) : SocketConnectionState()
+	public data class Disconnected(val error: Throwable? = null) : SocketConnectionState()
 
 	/**
 	 * A connection is currently in progress.
 	 */
-	public data object CONNECTING : SocketConnectionState()
+	public data object Connecting : SocketConnectionState()
 
 	/**
 	 * The last received message in an active connection.
 	 */
-	public data class MESSAGE(val message: String) : SocketConnectionState()
+	public data class Message(val message: String) : SocketConnectionState()
 }
