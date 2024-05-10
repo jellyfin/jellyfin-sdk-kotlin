88b98ef447cfe18f0f86e99ee2308f4732bdc17a
Niels van Velzen <git@ndat.nl>
2024-05-10 14:41:06 +0200

    Fix a kdoc typo

--- jellyfin-api/src/commonMain/kotlin/org/jellyfin/sdk/api/client/exception/ssl/HandshakeCertificateException.kt
+++ jellyfin-api/src/commonMain/kotlin/org/jellyfin/sdk/api/client/exception/ssl/HandshakeCertificateException.kt
@@ -2,7 +2,7 @@ package org.jellyfin.sdk.api.client.exception.ssl
 
 import org.jellyfin.sdk.api.client.exception.SecureConnectionException
 
-/***
+/**
  * An error occurred while attempting to establish a secure connection.
  * Indicates that the client and server could not negotiate the desired level of security or the certificate was
  * revoked.
