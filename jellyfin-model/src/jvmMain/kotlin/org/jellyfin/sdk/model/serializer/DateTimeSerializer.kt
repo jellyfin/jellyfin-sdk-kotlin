866d9764022369b0f851397c397d05dae0e495f4
Niels van Velzen <git@ndat.nl>
2026-01-22 16:17:44 +0100

    Remove zoneId parameter from DateTimeSerializer

--- jellyfin-model/src/jvmMain/kotlin/org/jellyfin/sdk/model/serializer/DateTimeSerializer.kt
+++ jellyfin-model/src/jvmMain/kotlin/org/jellyfin/sdk/model/serializer/DateTimeSerializer.kt
@@ -12,19 +12,22 @@ import java.time.ZonedDateTime
 import java.time.format.DateTimeFormatter
 import java.time.format.DateTimeParseException
 
-public actual class DateTimeSerializer(
-	private val zoneId: ZoneId = ZoneId.systemDefault(),
-) : KSerializer<DateTime> {
-	actual override val descriptor: SerialDescriptor = PrimitiveSerialDescriptor("org.jellyfin.LocalDateTime", PrimitiveKind.STRING)
+public actual class DateTimeSerializer : KSerializer<DateTime> {
+	actual override val descriptor: SerialDescriptor =
+		PrimitiveSerialDescriptor("org.jellyfin.LocalDateTime", PrimitiveKind.STRING)
 
-	actual override fun deserialize(decoder: Decoder): DateTime = try {
-		ZonedDateTime.parse(decoder.decodeString()).withZoneSameInstant(zoneId).toLocalDateTime()
+	actual override fun deserialize(
+		decoder: Decoder
+	): DateTime = try {
+		ZonedDateTime.parse(decoder.decodeString()).withZoneSameInstant(ZoneId.systemDefault()).toLocalDateTime()
 	} catch (err: DateTimeParseException) {
 		// Server will sometimes return 0001-01-01T00:00:00
 		// but java.time can't parse that
 		DateTime.MIN
 	}
 
-	actual override fun serialize(encoder: Encoder, value: DateTime): Unit =
-		encoder.encodeString(value.atZone(zoneId).format(DateTimeFormatter.ISO_OFFSET_DATE_TIME))
+	actual override fun serialize(
+		encoder: Encoder,
+		value: DateTime
+	): Unit = encoder.encodeString(value.atZone(ZoneId.systemDefault()).format(DateTimeFormatter.ISO_OFFSET_DATE_TIME))
 }
