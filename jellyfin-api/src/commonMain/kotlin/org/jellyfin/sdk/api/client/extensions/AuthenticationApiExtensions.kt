b6701ea5a95094d54e5e9eee1827d3dd4fc7c945
Niels van Velzen <git@ndat.nl>
2026-05-13 21:37:27 +0200

    Rename UserApiExtensions to AuthenticationApiExtensions

--- jellyfin-api/src/commonMain/kotlin/org/jellyfin/sdk/api/client/extensions/AuthenticationApiExtensions.kt
+++ jellyfin-api/src/commonMain/kotlin/org/jellyfin/sdk/api/client/extensions/AuthenticationApiExtensions.kt
@@ -1,7 +1,7 @@
 package org.jellyfin.sdk.api.client.extensions
 
 import org.jellyfin.sdk.api.client.Response
-import org.jellyfin.sdk.api.operations.UserApi
+import org.jellyfin.sdk.api.operations.AuthenticationApi
 import org.jellyfin.sdk.model.api.AuthenticateUserByName
 import org.jellyfin.sdk.model.api.AuthenticationResult
 import org.jellyfin.sdk.model.api.QuickConnectDto
@@ -9,7 +9,7 @@ import org.jellyfin.sdk.model.api.QuickConnectDto
 /**
  * Extension function for the authenticateUserByName operation that accepts the username and password directly
  */
-public suspend inline fun UserApi.authenticateUserByName(
+public suspend inline fun AuthenticationApi.authenticateUserByName(
 	username: String,
 	password: String,
 ): Response<AuthenticationResult> = authenticateUserByName(
@@ -22,7 +22,7 @@ public suspend inline fun UserApi.authenticateUserByName(
 /**
  * Extension function for the authenticateWithQuickConnect operation that accepts the secret directly
  */
-public suspend inline fun UserApi.authenticateWithQuickConnect(
+public suspend inline fun AuthenticationApi.authenticateWithQuickConnect(
 	secret: String,
 ): Response<AuthenticationResult> = authenticateWithQuickConnect(
 	data = QuickConnectDto(
